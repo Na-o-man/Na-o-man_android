686dc24b1d6c4a4ecdf899849fabc6148e7e9564
hangunhee39 <ghee0831@naver.com>
2024-07-16 02:27:03 +0900

    feat: 그룹 생성 init

---
+++ app/src/main/java/com/hgh/na_o_man/domain/model/Dummy.kt
@@ -0,0 +1,6 @@
+package com.hgh.na_o_man.domain.model
+
+data class Dummy(
+    val dummyString: String = "오오",
+    val dummyInt : Int = 55
+)
---
+++ app/src/main/java/com/hgh/na_o_man/presentation/ui/add/request/RequestInviteScreen.kt
@@ -0,0 +1,8 @@
+package com.hgh.na_o_man.presentation.ui.add.request
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RequestInviteScreen() {
+
+}
