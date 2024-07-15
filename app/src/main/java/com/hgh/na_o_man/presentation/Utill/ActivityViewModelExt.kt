3367d6ec363b17f85375cea4bead8ac23cd5b009
hangunhee39 <ghee0831@naver.com>
2024-07-16 02:27:51 +0900

    chore

---
+++ app/src/main/java/com/hgh/na_o_man/presentation/Utill/ActivityViewModelExt.kt
@@ -0,0 +1,13 @@
+package com.hgh.na_o_man.presentation.Utill
+
+import androidx.activity.ComponentActivity
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun getActivity() = LocalContext.current as ComponentActivity
+
+@Composable
+inline fun <reified VM : ViewModel> composableActivityViewModel(): VM = viewModel(getActivity())
---
+++ app/src/main/java/com/hgh/na_o_man/presentation/ui/main/mypage/MyPageScreen.kt
@@ -0,0 +1,8 @@
+package com.hgh.na_o_man.presentation.ui.main.mypage
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MyPageScreen() {
+
+}
