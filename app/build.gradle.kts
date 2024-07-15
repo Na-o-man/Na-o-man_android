b12b10c1935aa7a77b19dc38a6beea354c4ea8c3
hangunhee39 <ghee0831@naver.com>
2024-07-16 02:23:04 +0900

    chore: compose-navi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("androidx.navigation:navigation-compose:2.5.3")
+
 
     // hilt
     implementation("com.google.dagger:hilt-android:2.50")
