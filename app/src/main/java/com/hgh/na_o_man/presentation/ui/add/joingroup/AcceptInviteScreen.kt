204f0ab3ae1abdfa99c308580d277fcf7be4bc06
skyblue1232 <97932282+skyblue1232@users.noreply.github.com>
2024-07-20 05:49:48 +0900

    AddGroup 화면 만들기

--- app/src/main/java/com/hgh/na_o_man/presentation/ui/add/joingroup/AcceptInviteScreen.kt
+++ app/src/main/java/com/hgh/na_o_man/presentation/ui/add/joingroup/AcceptInviteScreen.kt
@@ -1,8 +1,9 @@
 package com.hgh.na_o_man.presentation.ui.add.joingroup
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
 
 @Composable
-fun AcceptInviteScreen() {
+fun AcceptInviteScreen(navController: NavController) {
 
 }
