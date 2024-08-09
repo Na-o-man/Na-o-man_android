42d26c43a4386c2bcc8f36ea19666f330e6880d4
hangunhee39 <ghee0831@naver.com>
2024-08-09 18:26:34 +0900

    feat: 사진 dto, model 수정

--- app/src/main/java/com/hgh/na_o_man/domain/model/photo/PhotoInfoModel.kt
+++ app/src/main/java/com/hgh/na_o_man/domain/model/photo/PhotoInfoModel.kt
@@ -1,8 +1,13 @@
 package com.hgh.na_o_man.domain.model.photo
 
 data class PhotoInfoModel(
+    val photoId : Long,
     val createdAt: String,
     val rawPhotoUrl: String,
     val w200PhotoUrl: String,
-    val w400PhotoUrl: String
-)
+    val w400PhotoUrl: String,
+    val isSelected : Boolean = false,
+    val isDownloaded : Boolean = false,
+) {
+    constructor() : this(-1,"","", "","")
+}
--- app/src/main/java/com/hgh/na_o_man/domain/repository/PhotoRepository.kt
+++ app/src/main/java/com/hgh/na_o_man/domain/repository/PhotoRepository.kt
@@ -8,7 +8,7 @@ import com.hgh.na_o_man.domain.model.photo.PhotoPreSignedModel
 import com.hgh.na_o_man.domain.model.photo.PhotoUploadCountModel
 
 interface PhotoRepository {
-    suspend fun postUpload(photoUrlListDto: PhotoUrlListDto) : RetrofitResult<PhotoUploadCountModel>
-    suspend fun postPreSignedUrl(photoNameListDto: PhotoNameListDto) : RetrofitResult<PhotoPreSignedModel>
-    suspend fun getPhotoAll(shareGroupId : Long, page : Int, size : Int, sort : String) : RetrofitResult<PhotoAllModel>
+    suspend fun postUpload(photoUrlListDto: PhotoUrlListDto): RetrofitResult<PhotoUploadCountModel>
+    suspend fun postPreSignedUrl(photoNameListDto: PhotoNameListDto): RetrofitResult<PhotoPreSignedModel>
+    suspend fun getPhotoAll(shareGroupId: Long, page: Int, size: Int): RetrofitResult<PhotoAllModel>
 }
