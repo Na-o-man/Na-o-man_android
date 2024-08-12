ba8273fa0889c37910367d3a8a7144004bf46fe4
hangunhee39 <ghee0831@naver.com>
2024-08-13 00:58:59 +0900

    피드백

--- app/src/main/java/com/hgh/na_o_man/domain/model/share_group/ProfileInfoModel.kt
+++ app/src/main/java/com/hgh/na_o_man/domain/model/share_group/ProfileInfoModel.kt
@@ -1,7 +1,7 @@
 package com.hgh.na_o_man.domain.model.share_group
 
 data class ProfileInfoModel(
-    val image: String?,
+    val image: String,
     val memberId: Long,
     val name: String,
     val profileId: Long
