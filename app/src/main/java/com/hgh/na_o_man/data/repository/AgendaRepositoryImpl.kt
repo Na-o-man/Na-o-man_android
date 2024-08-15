74a46df9cbb6eb009a907a556d0d9b368bdd4665
skyblue1232 <97932282+skyblue1232@users.noreply.github.com>
2024-08-15 16:52:41 +0900

    안건 API

--- app/src/main/java/com/hgh/na_o_man/data/repository/AgendaRepositoryImpl.kt
+++ app/src/main/java/com/hgh/na_o_man/data/repository/AgendaRepositoryImpl.kt
@@ -1,11 +1,18 @@
 package com.hgh.na_o_man.data.repository
 
 import com.hgh.na_o_man.data.dto.ApiResult
+import com.hgh.na_o_man.data.dto.agenda.request.AgendaRequestDto
+import com.hgh.na_o_man.data.dto.agenda.response.AgendaDeleteDto
+import com.hgh.na_o_man.data.dto.agenda.response.AgendaDetailInfoDto
 import com.hgh.na_o_man.data.dto.agenda.response.AgendaInfoListDto
+import com.hgh.na_o_man.data.dto.agenda.response.AgendaSuccessDto
 import com.hgh.na_o_man.data.source.remote.api.AgendasService
 import com.hgh.na_o_man.di.util.remote.RetrofitResult
 import com.hgh.na_o_man.di.util.remote.apiHandler
+import com.hgh.na_o_man.domain.model.agenda.AgendaDeleteModel
+import com.hgh.na_o_man.domain.model.agenda.AgendaDetailInfoModel
 import com.hgh.na_o_man.domain.model.agenda.AgendaInfoListModel
+import com.hgh.na_o_man.domain.model.agenda.AgendaSuccessModel
 import com.hgh.na_o_man.domain.repository.AgendaRepository
 import javax.inject.Inject
 
@@ -14,5 +21,14 @@ class AgendaRepositoryImpl @Inject constructor(
 ) : AgendaRepository{
     override suspend fun getAgenda(shareGroupId: Long, page: Int, size: Int): RetrofitResult<AgendaInfoListModel> {
         return apiHandler({ api.getAllAgenda(shareGroupId,page, size)}) {response : ApiResult<AgendaInfoListDto> -> response.data.toModel()}
+    }
+    override suspend fun postAgenda(agendaRequestDto: AgendaRequestDto): RetrofitResult<AgendaSuccessModel> {
+        return apiHandler({ api.postCreateAgenda(agendaRequestDto) }) {response : ApiResult<AgendaSuccessDto> -> response.data.toModel()}
+    }
+    override suspend fun getSearch(agendaId: Long): RetrofitResult<AgendaDetailInfoModel> {
+        return apiHandler({ api.getSearchAgenda(agendaId) }) {response : ApiResult<AgendaDetailInfoDto> -> response.data.toModel()}
+    }
+    override suspend fun deleteAgenda(agendaId: Long): RetrofitResult<AgendaDeleteModel> {
+        return apiHandler({ api.deleteSpecificAgenda(agendaId) }) {response : ApiResult<AgendaDeleteDto> -> response.data.toModel()}
     }
 }
