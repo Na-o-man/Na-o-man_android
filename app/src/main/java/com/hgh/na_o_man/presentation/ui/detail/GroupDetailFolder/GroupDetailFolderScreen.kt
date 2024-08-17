90be3a482355cf8ac748fb20c94af85c26d73e69
hangunhee39 <ghee0831@naver.com>
2024-08-17 19:22:49 +0900

    feat: 폴더 디자인

--- app/src/main/java/com/hgh/na_o_man/presentation/ui/detail/GroupDetailFolder/GroupDetailFolderScreen.kt
+++ app/src/main/java/com/hgh/na_o_man/presentation/ui/detail/GroupDetailFolder/GroupDetailFolderScreen.kt
@@ -9,6 +9,8 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -21,6 +23,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -33,6 +37,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.vectorResource
@@ -59,6 +64,8 @@ import com.hgh.na_o_man.presentation.component.groupdetail.GroupInfo
 import com.hgh.na_o_man.presentation.component.groupdetail.SmallFolder
 import com.hgh.na_o_man.presentation.ui.detail.ALL_PHOTO_ID
 import com.hgh.na_o_man.presentation.ui.detail.GroupDetailActivity.Companion.GROUP_DETAIL
+import kotlinx.coroutines.launch
+import kotlin.math.abs
 
 
 @ExperimentalPagerApi
@@ -73,6 +80,7 @@ fun GroupDetailFolderScreen(
 ) {
     val viewState by viewModel.viewState.collectAsState()
     val context = LocalContext.current as Activity
+    val coroutineScope = rememberCoroutineScope()
     val imagePickerLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.PickMultipleVisualMedia()
     ) { uris: List<Uri> ->
@@ -90,7 +98,7 @@ fun GroupDetailFolderScreen(
         viewModel.effect.collect { effect ->
             when (effect) {
                 is GroupDetailFolderContract.GroupDetailFolderSideEffect.NaviPhotoList -> {
-                    navigationPhotoList(effect.groupId, effect.profiledId ,effect.memberId)
+                    navigationPhotoList(effect.groupId, effect.profiledId, effect.memberId)
                 }
 
                 is GroupDetailFolderContract.GroupDetailFolderSideEffect.NaviVote -> {
@@ -113,6 +121,18 @@ fun GroupDetailFolderScreen(
 
     // 페이저용
     val pagerState = rememberPagerState(initialPage = 0)
+
+    LaunchedEffect(pagerState.currentPage) {
+        if (pagerState.currentPage == 0) {
+            coroutineScope.launch {
+                pagerState.scrollToPage(viewState. * 1000)
+            }
+        } else if (pagerState.currentPage == viewState.groupDetail?.profileInfoList?.size ?:0 + 2 * 1000 + actualPageCount - 1) {
+            coroutineScope.launch {
+                pagerState.scrollToPage(actualPageCount * 1000 + actualPageCount)
+            }
+        }
+    }
 
     when (viewState.loadState) {
         LoadState.LOADING -> {
@@ -165,12 +185,13 @@ fun GroupDetailFolderScreen(
                             }
                         }
 
-                        Spacer(modifier = Modifier.height(20.dp))
+                        Spacer(modifier = Modifier.height(40.dp))
 
                         viewState.groupDetail?.let { groupDetail ->
                             // Filter out folders without memberId
 
-                            val filteredProfileInfoList = groupDetail.profileInfoList.filter { it.memberId > 0 }
+                            val filteredProfileInfoList =
+                                groupDetail.profileInfoList.filter { it.memberId > 0 }
 
                             val folderDummyList = filteredProfileInfoList.map {
                                 FolderDummy(
@@ -189,43 +210,75 @@ fun GroupDetailFolderScreen(
                                     name = "all"
                                 )
                             )
-                            HorizontalPager(
-                                count = folderList.size,
-                                state = pagerState,
-                                contentPadding = PaddingValues(horizontal = 40.dp), // Set padding to create partial view
+                            Box(
                                 modifier = Modifier
+                                    .height(264.dp)
                                     .fillMaxWidth()
-                                    .height(220.dp)
-                            ) { page ->
-                                val scale by animateFloatAsState(targetValue = 1.1f)
-                                Box(
-                                    contentAlignment = Alignment.Center,
-                                    modifier = Modifier
-                                        .scale(scale)
-                                ) {
-                                    val folderInfo = folderList[page]
+                            ) {
+                                Icon(
+                                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_round_folder_600),
+                                    contentDescription = null,
+                                    tint = Color.Unspecified,
+                                    modifier = Modifier.width(800.dp).align(Alignment.BottomCenter)
+                                )
 
-                                    Bigfolder(
-                                        folderInfo = folderInfo,
-                                        onClick = {
-                                            val memberId = filteredProfileInfoList.getOrNull(page)?.memberId ?: -1
-//                                            val profileId = filteredProfileInfoList.getOrNull(page)?.profileId ?: 100L
-                                            val profileId = when {
-                                                filteredProfileInfoList.getOrNull(page)?.profileId != null -> filteredProfileInfoList[page].profileId
-                                                folderInfo.name == "all" -> 100L
-                                                folderInfo.name == "others" -> 101L
-                                                else -> -1L
+                                HorizontalPager(
+                                    count = folderList.size,
+                                    state = pagerState,
+                                    contentPadding = PaddingValues(horizontal = 70.dp), // Set padding to create partial view
+                                    modifier = Modifier.matchParentSize(),
+                                    itemSpacing = 0.dp,
+
+                                ) { page ->
+
+                                    val currentPage = pagerState.currentPage
+                                    val pageOffset = pagerState.currentPageOffset
+                                    val pagePosition = page - currentPage - pageOffset
+
+                                    // 페이지가 중앙에 가까울수록 더 커지도록 설정
+                                    val targetScale = 0.8f + (1.2f - 0.8f) * (1 - abs(pagePosition))
+
+                                    // 스케일 애니메이션
+                                    Box(
+                                        contentAlignment = Alignment.Center,
+                                        modifier = Modifier
+                                            .fillMaxSize()
+                                            .graphicsLayer {
+                                                scaleX = targetScale
+                                                scaleY = targetScale
+                                                transformOrigin =
+                                                    androidx.compose.ui.graphics.TransformOrigin(
+                                                        0.5f,
+                                                        1f
+                                                    ) // 변환 원점을 아래쪽 중앙으로 설정
                                             }
-                                            navigationPhotoList(
-                                                groupDetail.shareGroupId,
-                                                profileId,
-                                                memberId
-                                            )
-                                        }
-                                    )
+                                    ) {
+                                        val folderInfo = folderList[page]
+
+                                        Bigfolder(
+                                            folderInfo = folderInfo,
+                                            onClick = {
+                                                val memberId =
+                                                    filteredProfileInfoList.getOrNull(page)?.memberId
+                                                        ?: -1
+//                                            val profileId = filteredProfileInfoList.getOrNull(page)?.profileId ?: 100L
+                                                val profileId = when {
+                                                    filteredProfileInfoList.getOrNull(page)?.profileId != null -> filteredProfileInfoList[page].profileId
+                                                    folderInfo.name == "all" -> 100L
+                                                    folderInfo.name == "others" -> 101L
+                                                    else -> -1L
+                                                }
+                                                navigationPhotoList(
+                                                    groupDetail.shareGroupId,
+                                                    profileId,
+                                                    memberId
+                                                )
+                                            }
+                                        )
+                                    }
                                 }
                             }
-                            
+
                             Spacer(modifier = Modifier.height(15.dp))
 
                             HorizontalPagerIndicator(
